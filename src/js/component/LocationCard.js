0dcae47744e1c20e4d49808f6d8e64d663be5a9c
Dan Zagar <122122116+dzagar88@users.noreply.github.com>
2023-04-27 18:31:29 +0000

    minor detail changes

--- src/js/component/LocationCard.js
+++ src/js/component/LocationCard.js
@@ -9,7 +9,7 @@ export const LocationCard = ({location}) => {
     <div className="card" style={{ minWidth: "18rem", maxWidth:"18rem" }}>
       <img src={`https://www.verance.com/app/uploads/2017/01/400x200.png`} className="card-img-top" alt="..." />
       <div className="card-body" style={{ backgroundColor: "white" }}>
-        <h5 className="card-title" style={{ backgroundColor: "white" }}><strong style={{ backgroundColor: "white" }}>{"Name: "}</strong>{location.name}</h5>
+        <h5 className="card-title" style={{ backgroundColor: "white" }}><strong style={{ backgroundColor: "white" }}>{"Location: "}</strong>{location.name}</h5>
         <ul className="card-text" style={{ backgroundColor: "white" }}>
         <li style={{ backgroundColor: "white" }}><strong style={{ backgroundColor: "white" }}>{"Type: "}</strong>{location.type}</li>
         <li style={{ backgroundColor: "white" }}><strong style={{ backgroundColor: "white" }}>{"Dimension: "}</strong>{location.dimension}</li>
